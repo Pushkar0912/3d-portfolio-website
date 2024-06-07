1d386941ecff10b7cb98517f1775954e5b9e724d
Pushkar Patil <push2001official@gmail.com>
2024-06-07 05:22:57 -0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,63 @@
+import styled, { ThemeProvider } from "styled-components"
+import {darkTheme} from "./utils/Themes"
+import Navbar from "./components/Navbar";
+import { BrowserRouter } from "react-router-dom";
+import Hero from "./components/sections/Hero";
+import Skills from "./components/sections/Skills";
+import Experience from "./components/sections/Experience";
+import Projects from "./components/sections/Projects";
+import Education from "./components/sections/Education";
+import Footer from "./components/sections/Footer";
+import StartCanvas from "./components/canvas/Stars";
+
+
+const Body = styled.div`
+  background-color:black;
+  color:${({theme})=>theme.text_primary};
+  width:100%;
+  height:100vh;
+  overflow-x:hidden;
+  position:relative;
+`;
+const Wrapper = styled.div`
+  padding-bottom: 100px;
+  background: linear-gradient(
+      38.73deg,
+      rgba(204, 0, 187, 0.15) 0%,
+      rgba(201, 32, 184, 0) 50%
+    ),
+    linear-gradient(
+      141.27deg,
+      rgba(0, 70, 209, 0) 50%,
+      rgba(0, 70, 209, 0.15) 100%
+    );
+  width: 100%;
+  clip-path: polygon(0 0, 100% 0, 100% 100%, 30% 98%, 0 100%);
+`;
+
+function App() {
+  return (
+    <ThemeProvider theme={darkTheme}>
+      <BrowserRouter>
+        <Navbar />
+        <Body>
+          <StartCanvas />
+          <div>
+            <Hero />
+            <Wrapper>
+              <Skills />
+              <Experience />
+            </Wrapper>
+            <Projects />
+            <Wrapper>
+              <Education />
+            </Wrapper>
+            <Footer />
+          </div>
+        </Body>
+      </BrowserRouter>
+    </ThemeProvider>
+  );
+}
+
+export default App;
